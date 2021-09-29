643cc95d5c5cd93f2f2c1629740646503a5c5f38
JunichiEnomoto <jenomoto@netsis.jp>
2021-09-29 14:13:22 +0900

    test

--- UnityProject/Assets/Scripts/AtlasHook.cs
+++ UnityProject/Assets/Scripts/AtlasHook.cs
@@ -8,21 +8,10 @@ using UnityEngine.U2D;
 public class AtlasHook : MonoBehaviour
 {
     public BundledAssetPath _bundledAssetPath; 
-    public UnityEvent _completeInitialize;
-
-    IEnumerator Start()
-    {
-        //show log message
-        BundleManager.LogMessages = true;
-        //show some ongui elements for debugging
-        BundleManager.ShowDebugGUI = true;
-        yield return BundleManager.Initialize();
-        DontDestroyOnLoad(this);
-        _completeInitialize?.Invoke();
-    }
 
     void OnEnable()
     {
+        DontDestroyOnLoad(this);
         Debug.Log($"SpriteAtlasManager.atlasRequested += OnAtlasRequested;");
         SpriteAtlasManager.atlasRequested += OnAtlasRequested;
     }
@@ -36,19 +25,28 @@ public class AtlasHook : MonoBehaviour
     void OnAtlasRequested(string tag, System.Action<SpriteAtlas> atlasCallback)
     {
         Debug.Log($"OnAtlasRequested {tag}");
-        var request1 = _bundledAssetPath.Load<SpriteAtlas>();
-        
-        var request2 = BundleManager.Load<SpriteAtlas>(
-            AssetBundleConst.ABConst_CommonAtlas.ASSETBUNDLE_NAME,
-            AssetBundleConst.ABConst_CommonAtlas.COMMONATLAS_SPRITEATLAS);
+        StartCoroutine(LoadAtlas(tag, atlasCallback));
+    }
+    
+    IEnumerator LoadAtlas(string tag, System.Action<SpriteAtlas> atlasCallback)
+    {
+        if (!BundleManager.Initialized)
+        {
+            //show log message
+            BundleManager.LogMessages = true;
+            //show some ongui elements for debugging
+            BundleManager.ShowDebugGUI = true;
+            Debug.Log($"{Time.frameCount} > BundleManager.Initialize Start");
+            yield return BundleManager.Initialize();
+            Debug.Log($"{Time.frameCount} > BundleManager.Initialize End");
+        }
 
-        var name1 = request1 == null ? "null" : request1.name;
-        Debug.Log($"LoadSpriteAtlas request1 {name1}");
-        var name2 = request2 == null ? "null" : request2.name;
-        Debug.Log($"LoadSpriteAtlas request2 {name2}");
-        if(request1!=null)
-            atlasCallback.Invoke(request1);
-        if(request2!=null)
-            atlasCallback.Invoke(request2);
+        Debug.Log($"{Time.frameCount} > bundledAssetPath.LoadAsync<SpriteAtlas> Start");
+        var request = _bundledAssetPath.LoadAsync<SpriteAtlas>();
+        yield return request;
+        Debug.Log($"{Time.frameCount} > bundledAssetPath.LoadAsync<SpriteAtlas> End");
+        
+        if(request.Asset!=null)
+            atlasCallback(request.Asset);
     }
 }
--- UnityProject/Assets/Scripts/BundleTest.cs
+++ UnityProject/Assets/Scripts/BundleTest.cs
@@ -15,7 +15,9 @@ public class BundleTest : MonoBehaviour
             BundleManager.LogMessages = true;
             //show some ongui elements for debugging
             BundleManager.ShowDebugGUI = true;
+            Debug.Log($"{Time.frameCount} > BundleManager.Initialize Start");
             yield return BundleManager.Initialize();
+            Debug.Log($"{Time.frameCount} > BundleManager.Initialize End");
         }
 
         Debug.Log(Utility.CombinePath(BundleManager.RemoteURL, AssetbundleBuildSettings.ManifestFileName).Replace('\\', '/'));
